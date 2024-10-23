036ef12a2c8c2dc7a6df88abdecce448efd5b905
AnBinh1703 <dan1314705@gmail.com>
2024-10-24 06:23:54 +0700

    add https://cors-anywhere.herokuapp.com/ for can deploy

--- src/api/config.js
+++ src/api/config.js
@@ -1,22 +1,54 @@
+// import axios from "axios";
+// //const baseUrl = "http://localhost:5275";
+// const baseUrl =
+//   "https://cors-anywhere.herokuapp.com/http://14.225.220.108:2602";
+// const config = {
+//   baseUrl,
+//   timeout: 300000,
+// };
+// const api = axios.create(config);
+// api.defaults.baseURL = baseUrl;
+// const handleBefore = (config) => {
+//   const token = localStorage.getItem("accessToken")?.replaceAll('"', "");
+//   config.headers["Authorization"] = `Bearer ${token}`;
+//   return config;
+// };
+// const handleError = (error) => {
+//   console.log(error);
+//   return;
+// };
+// api.interceptors.request.use(handleBefore, handleError);
+// // api.interceptors.response.use(null, handleError);
+
+// export default api;
 import axios from "axios";
-//const baseUrl = "http://localhost:5275";
-const baseUrl = "http://14.225.220.108:2602";
+
+// const baseUrl = "http://localhost:5275";
+const baseUrl =
+  "https://cors-anywhere.herokuapp.com/http://14.225.220.108:2602";
+
 const config = {
-  baseUrl,
+  baseURL: baseUrl,
   timeout: 300000,
+  headers: {
+    "x-requested-with": "XMLHttpRequest", // Thêm header này để yêu cầu hợp lệ
+  },
 };
+
 const api = axios.create(config);
-api.defaults.baseURL = baseUrl;
+
 const handleBefore = (config) => {
   const token = localStorage.getItem("accessToken")?.replaceAll('"', "");
   config.headers["Authorization"] = `Bearer ${token}`;
+  config.headers["origin"] = window.location.origin; // Thêm header origin
   return config;
 };
+
 const handleError = (error) => {
   console.log(error);
   return;
 };
+
 api.interceptors.request.use(handleBefore, handleError);
-// api.interceptors.response.use(null, handleError);
 
 export default api;
