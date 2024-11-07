51dc43cde0b80a52d423dbfc904d1acb1023e37a
AnBinh1703 <dan1314705@gmail.com>
2024-11-07 07:27:15 +0700

    feat: add search functionality and highlight for order list, update order status and type rendering, update manage order of supllier

--- src/pages/Supllier/Order/OrderListBySuplier.jsx
+++ src/pages/Supllier/Order/OrderListBySuplier.jsx
@@ -1,5 +1,17 @@
-import { Button, message, Modal, Spin, Table } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import {
+  Button,
+  DatePicker,
+  Input,
+  message,
+  Modal,
+  Spin,
+  Table,
+  Tag,
+} from "antd";
+import moment from "moment";
 import React, { useEffect, useState } from "react";
+import Highlighter from "react-highlight-words";
 import { useSelector } from "react-redux";
 import { getSupplierIdByAccountId } from "../../../api/accountApi";
 import { getOrderOfSupplierId } from "../../../api/orderApi";
@@ -15,27 +27,29 @@ const OrderListBySupplier = ({ refresh }) => {
   const [supplierId, setSupplierId] = useState(null);
   const [isTrackingModalVisible, setIsTrackingModalVisible] = useState(false);
   const [selectedOrder, setSelectedOrder] = useState(null);
+  const [searchText, setSearchText] = useState("");
+  const [searchedColumn, setSearchedColumn] = useState("");
 
   const orderStatusMap = {
-    0: "Chờ xử lý",
-    1: "Đã phê duyệt",
-    2: "Hoàn thành",
-    3: "Đã đặt",
-    4: "Đã giao hàng",
-    5: "Đã nhận",
-    6: "Đã hủy",
-    7: "Thanh toán",
+    0: { text: "Chờ xử lý", color: "blue" },
+    1: { text: "Đã phê duyệt", color: "green" },
+    2: { text: "Hoàn thành", color: "gold" },
+    3: { text: "Đã đặt", color: "purple" },
+    4: { text: "Đã giao hàng", color: "cyan" },
+    5: { text: "Đã nhận", color: "lime" },
+    6: { text: "Đã hủy", color: "red" },
+    7: { text: "Thanh toán", color: "volcano" },
   };
 
   const orderTypeMap = {
-    0: "Mua",
-    1: "Thuê",
+    0: { text: "Mua", color: "blue" },
+    1: { text: "Thuê", color: "green" },
   };
 
   const deliveryStatusMap = {
-    0: "Đến cửa hàng lấy hàng",
-    1: "Cửa hàng giao hàng",
-    2: "Đã trả lại",
+    0: { text: "Đến cửa hàng lấy hàng", color: "blue" },
+    1: { text: "Cửa hàng giao hàng", color: "green" },
+    2: { text: "Đã trả lại", color: "red" },
   };
 
   // Lấy ID Nhà cung cấp
@@ -102,49 +116,201 @@ const OrderListBySupplier = ({ refresh }) => {
     setSelectedOrder(null);
   };
 
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ marginBottom: 8, display: "block" }}
+        />
+        <Button
+          type="primary"
+          onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          icon={<SearchOutlined />}
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          Search
+        </Button>
+        <Button
+          onClick={() => handleReset(clearFilters)}
+          size="small"
+          style={{ width: 90 }}
+        >
+          Reset
+        </Button>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined style={{ color: filtered ? "#1890ff" : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        ? record[dataIndex]
+            .toString()
+            .toLowerCase()
+            .includes(value.toLowerCase())
+        : "",
+    onFilterDropdownVisibleChange: (visible) => {
+      if (visible) {
+        setTimeout(() => document.getElementById("search-input").select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ""}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  const getColumnDateSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <div style={{ padding: 8 }}>
+        <DatePicker
+          onChange={(date, dateString) =>
+            setSelectedKeys(dateString ? [dateString] : [])
+          }
+          style={{ marginBottom: 8, display: "block" }}
+        />
+        <Button
+          type="primary"
+          onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          icon={<SearchOutlined />}
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          Search
+        </Button>
+        <Button
+          onClick={() => handleReset(clearFilters)}
+          size="small"
+          style={{ width: 90 }}
+        >
+          Reset
+        </Button>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined style={{ color: filtered ? "#1890ff" : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        ? moment(record[dataIndex]).format("DD-MM-YYYY") === value
+        : "",
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ""}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText("");
+  };
+
   const columns = [
     {
       title: "Mã đơn hàng",
       dataIndex: "orderID",
       key: "orderID",
+      sorter: (a, b) => a.orderID.localeCompare(b.orderID),
+      ...getColumnSearchProps("orderID"),
     },
     {
       title: "Mã tài khoản",
       dataIndex: "accountID",
       key: "accountID",
+      sorter: (a, b) => a.accountID.localeCompare(b.accountID),
+      ...getColumnSearchProps("accountID"),
     },
     {
       title: "Ngày đặt hàng",
       dataIndex: "orderDate",
       key: "orderDate",
+      sorter: (a, b) => new Date(a.orderDate) - new Date(b.orderDate),
+      render: (date) => moment(date).format("DD/MM/YYYY HH:mm:ss"),
     },
     {
       title: "Trạng thái đơn hàng",
       dataIndex: "orderStatus",
       key: "orderStatus",
-      render: (status) => orderStatusMap[status],
+      render: (status) => {
+        const statusInfo = orderStatusMap[status];
+        return statusInfo ? (
+          <Tag color={statusInfo.color}>{statusInfo.text}</Tag>
+        ) : null;
+      },
+      sorter: (a, b) => a.orderStatus - b.orderStatus,
     },
     {
       title: "Tổng số tiền",
       dataIndex: "totalAmount",
       key: "totalAmount",
+      sorter: (a, b) => a.totalAmount - b.totalAmount,
     },
     {
       title: "Loại đơn hàng",
       dataIndex: "orderType",
       key: "orderType",
-      render: (type) => orderTypeMap[type],
+      render: (type) => {
+        const typeInfo = orderTypeMap[type];
+        return typeInfo ? (
+          <Tag color={typeInfo.color}>{typeInfo.text}</Tag>
+        ) : null;
+      },
+      sorter: (a, b) => a.orderType - b.orderType,
     },
     {
       title: "Địa chỉ giao hàng",
       dataIndex: "shippingAddress",
       key: "shippingAddress",
+      sorter: (a, b) => a.shippingAddress.localeCompare(b.shippingAddress),
     },
     {
       title: "Phương thức giao hàng",
       dataIndex: "deliveryMethod",
       key: "deliveryMethod",
-      render: (status) => deliveryStatusMap[status],
+      render: (status) => {
+        const deliveryInfo = deliveryStatusMap[status];
+        return deliveryInfo ? (
+          <Tag color={deliveryInfo.color}>{deliveryInfo.text}</Tag>
+        ) : null;
+      },
+      sorter: (a, b) => a.deliveryMethod - b.deliveryMethod,
     },
     {
       title: "Hành động",
