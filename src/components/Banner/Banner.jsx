894a9867691005b2b7bbb4b830a409d1d01c3a2b
AnBinh1703 <dan1314705@gmail.com>
2024-11-12 16:40:56 +0700

    enhance sidebar and banner UI, update order status mapping

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,11 +4,11 @@ const Banner = ({ title }) => {
       <img
         src={productBg}
         alt="Product-bg"
-        className="w-200 h-60 object-cover"
+        className="w-full h-auto object-cover"
       />
       <div className="absolute inset-0 bg-black bg-opacity-60 flex justify-center items-center">
         <div className="text-center">
-          <h2 className="text-white text-3xl">{title}</h2>
+          <h2 className="text-white text-3xl md:text-5xl">{title}</h2>
         </div>
       </div>
     </div>
