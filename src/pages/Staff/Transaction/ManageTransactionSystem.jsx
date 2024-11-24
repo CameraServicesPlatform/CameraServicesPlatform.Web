b32e80a76aee5a096e2024cabb20020aeb1bdff8
AnBinh1703 <dan1314705@gmail.com>
2024-11-24 17:39:19 +0700

    feat: enhance transaction management; add CreateStaffRefundSupplier component, update refund status mapping, and improve error handling for supplier and account data fetching

--- src/pages/Staff/Transaction/ManageTransactionSystem.jsx
+++ src/pages/Staff/Transaction/ManageTransactionSystem.jsx
@@ -1,6 +1,7 @@
 import { Tabs } from "antd";
 import React, { useState } from "react";
 import CreateStaffRefundMember from "./CreateStaffRefundMember";
+import CreateStaffRefundSupplier from "./CreateStaffRefundSupplier";
 import HistoryTransactionList from "./HistoryTransactionList";
 import TransactionList from "./TransactionList";
 const ManageTransactionSystem = () => {
@@ -20,14 +21,21 @@ const ManageTransactionSystem = () => {
     },
     {
       key: "3",
-      label: "Tạo Giao Dịch Hoàn Trả Cho Nhà cung cấp",
+      label: "Tạo Giao Dịch Hoàn Trả Cho Khách Hàng",
       children: <CreateStaffRefundMember refresh={refreshList} />,
+    },
+    {
+      key: "4",
+      label: "Tạo Giao Dịch Hoàn Trả Cho Nhà cung cấp",
+      children: <CreateStaffRefundSupplier refresh={refreshList} />,
     },
   ];
 
   return (
     <div className="p-6 bg-white rounded-lg shadow-lg">
-      <h1 className="text-3xl font-semibold mb-6">Quản Lí Giao Dịch Sản Phẩm</h1>{" "}
+      <h1 className="text-3xl font-semibold mb-6">
+        Quản Lí Giao Dịch Sản Phẩm
+      </h1>{" "}
       <Tabs defaultActiveKey="1" items={tabItems} />
     </div>
   );
